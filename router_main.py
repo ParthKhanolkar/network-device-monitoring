dca9bf7d3da2ef8e3afcd16751ee09edb5864b57
Parth Khanolkar <khr0nus144@gmail.com>
2023-01-26 18:31:04 +0530

    imported sources

--- router_main.py
+++ router_main.py
@@ -4,8 +4,15 @@ import Router.router_display.tech_support as tech_support
 import Router.router_display.interfaces_display as interfaces_display
 import Router.router_display.syslog as syslog
 import Router.router_display.configuration_display as configuration_display
+import Router.router_display.ip_protocol_display as ip_protocol_display
+import Router.router_display.ip_route_display as ip_route_display
+import Router.router_display.LLDP_display as LLDP_display
+import Router.router_display.NTP_display as NTP_display
 
 import Router.router_configure.ip_routes as ip_routes
 import Router.router_configure.interface_config as interface_config
+import Router.router_configure.LLDP_config as LLDP_config
+import Router.router_configure.NTP_config as NTP_config
+import Router.router_configure.security_configuration as security_configuration
 
 print(cisco_device['device_type'])
--- switch_main.py
+++ switch_main.py
@@ -6,9 +6,13 @@ import Switch.switch_display.dis_mac as dis_mac
 import Switch.switch_display.interfaces_display as interfaces_display
 import Switch.switch_display.tech_support as tech_support
 import Switch.switch_display.vlan_display as vlan_display
+import Switch.switch_display.etherchannel_display as etherchannel_display
+import Switch.switch_display.LLDP_display as LLDP_display
+import Switch.switch_display.NTP_display as NTP_display
 
 import Switch.switch_configure.conf_mac as conf_mac
 import Switch.switch_configure.interface_config as interface_config
 import Switch.switch_configure.vlan_config as vlan_config
-
-
+import Switch.switch_configure.etherchannel_config as etherchannel_config
+import Switch.switch_configure.LLDP_config as LLDP_config
+import Switch.switch_configure.NTP_config as NTP_config
