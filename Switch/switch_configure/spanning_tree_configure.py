d1e37c7ed3667cbc46783234c8721a67e32a7ff8
Parth Khanolkar <khr0nus144@gmail.com>
2023-04-26 13:25:01 +0530

    beproject

--- Switch/switch_configure/spanning_tree_configure.py
+++ Switch/switch_configure/spanning_tree_configure.py
@@ -1,38 +1,56 @@
-from connect import *
+from netmiko import ConnectHandler
 
 def enable_portfast(interface):
+    from connect import get_ssh_connect
+    ssh_connect = get_ssh_connect()
     ssh_connect.enable()
     ssh_connect.send_config_set(['interface ' + interface, 'spanning-tree portfast'])
 
 def enable_bpdu_guard(interface):
+    from connect import get_ssh_connect
+    ssh_connect = get_ssh_connect()
     ssh_connect.enable()
     ssh_connect.send_config_set(['interface ' + interface, 'spanning-tree bpduguard enable'])
 
 def enable_bpdu_guard_all():
+    from connect import get_ssh_connect
+    ssh_connect = get_ssh_connect()
     ssh_connect.enable()
     ssh_connect.send_config_set(['spanning-tree portfast bpduguard default'])
 
 def pvst_mode(mode):
+    from connect import get_ssh_connect
+    ssh_connect = get_ssh_connect()
     ssh_connect.enable()
     ssh_connect.send_config_set(['spanning-tree mode ' + mode])
 
 def spanning_tree_vlan_priority(vlan_number,priority):
+    from connect import get_ssh_connect
+    ssh_connect = get_ssh_connect()
     ssh_connect.enable()
     ssh_connect.send_config_set(['spanning-tree vlan ' + vlan_number + 'priority ' + priority])
 
 def spanning_tree_vlan_root(vlan_number,root_priority):
+    from connect import get_ssh_connect
+    ssh_connect = get_ssh_connect()
     ssh_connect.enable()
     ssh_connect.send_config_set(['spanning-tree vlan ' + vlan_number + 'root ' + root_priority])
 
 def spanning_tree_vlan_cost(vlan_number,cost):
+    from connect import get_ssh_connect
+    ssh_connect = get_ssh_connect()
     ssh_connect.enable()
     ssh_connect.send_config_set(['spanning-tree vlan ' + vlan_number + 'cost ' + cost])
 
 def spanning_tree_vlan_port_priority(vlan_number,port_priority):
+    from connect import get_ssh_connect
+    ssh_connect = get_ssh_connect()
     ssh_connect.enable()
     ssh_connect.send_config_set(['spanning-tree vlan ' + vlan_number + 'port-priority ' + port_priority])
 
 def rstp_link_type(link_type):
+    from connect import get_ssh_connect
+    ssh_connect = get_ssh_connect()
     ssh_connect.enable()
     ssh_connect.send_config_set(['spanning-tree link-type ' + link_type])
 
--- Switch/switch_display/etherchannel_display.py
+++ Switch/switch_display/etherchannel_display.py
@@ -3,14 +3,14 @@ from netmiko import ConnectHandler
 def etherchannel_loadbalance_display():
     from connect import get_ssh_connect
     ssh_connect = get_ssh_connect()
-    ssh_connect.send_command('show etherchannel load-balance')
+    return ssh_connect.send_command('show etherchannel load-balance')
 
 def etherchannel_summary_display():
     from connect import get_ssh_connect
     ssh_connect = get_ssh_connect()
-    ssh_connect.send_command('show etherchannel summary')
+    return ssh_connect.send_command('show etherchannel summary')
 
 def etherchannel_portchannel_display():
     from connect import get_ssh_connect
     ssh_connect = get_ssh_connect()
-    ssh_connect.send_command('show etherchannel port-channel')
+    return ssh_connect.send_command('show etherchannel port-channel')
